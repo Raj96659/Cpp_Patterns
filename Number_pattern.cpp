c8b2e5fdc1aba10771c425c845a18a0a8381f024
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2023-08-09 17:11:12 +0530

    Create Number_pattern.cpp

---
+++ Number_pattern.cpp
@@ -0,0 +1,24 @@
+/*
+
+ 1
+ 2 2
+ 3 3 3
+ 4 4 4 4
+ 5 5 5 5 5
+
+*/
+
+#include<iostream>
+using namespace std;
+main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<" "<<i;
+        }
+        cout<<"\n";
+    }
+}
