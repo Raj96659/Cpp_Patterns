67f316b14b3bcbbdc7aeeb8cd60337a119064533
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2023-08-09 17:44:51 +0530

    Create Star_pattern_2.cpp

---
+++ Star_pattern_2.cpp
@@ -0,0 +1,23 @@
+/*
+
+ * * * *
+ * * *
+ * *
+ *
+
+*/
+
+#include<iostream>
+using namespace std;
+main()
+{
+    int i,j;
+    for(i=4;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<" *";
+        }
+        cout<<"\n";
+    }
+}
